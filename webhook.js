3216e3c1cce31540525ebc52423739550065224b
bianzh <654281997@qq.com>
2021-10-10 22:59:49 +0800

    webhook

---
+++ webhook.js
@@ -0,0 +1,18 @@
+let http = require('http')
+
+let server = http.createServer(function(req, res) {
+	let {
+		method,
+		url
+	} = req
+	if (method === 'POST' && url === 'webhook') {
+		res.setHeader('Content-Type', 'application/json')
+		res.end(JSON.stringify(ok: true))
+	} else {
+		res.end('Not found')
+	}
+})
+
+server.listen(4000, () => {
+	console.log('webhook服务已经在4000窗口上启动...')
+})
